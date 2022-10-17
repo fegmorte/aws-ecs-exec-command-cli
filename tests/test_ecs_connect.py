698863af02e23611c92ad0093e0e0791a0f94a93
Frédéric EGMORTE <frederic@yescapa.com>
2022-10-17 08:52:11 +0200

    add tests

---
+++ tests/test_ecs_connect.py
@@ -0,0 +1,12 @@
+# tests/test_ecs_connect.py
+
+from typer.testing import CliRunner
+
+from ecs_connect import __app_name__, __version__, cli
+
+runner = CliRunner()
+
+def test_version():
+    result = runner.invoke(cli.app, ["--version"])
+    assert result.exit_code == 0
+    assert f"{__app_name__} v{__version__}\n" in result.stdout
