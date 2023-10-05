ea1fca8e02ba151773b8e549b00eeb6ede11c56b
Frédéric EGMORTE <frederic@yescapa.com>
2023-10-05 16:29:01 +0200

    fix: Fix user to connect with

--- ecs_connect/cli.py
+++ ecs_connect/cli.py
@@ -1,7 +1,8 @@
-import configparser
 import os
+import multiprocessing
 import sys
 import subprocess
+import time
 import typer
 
 from ecs_connect import __app_name__, __version__
@@ -93,68 +94,40 @@ def list_task():
 def ecs_connect():
     """Connect to an ECS Fargate container"""
     try:
-        # Check if last config exists
-        if os.path.isfile("config.ini"):
-            use_last_config = make_choice(choice="use_last_config")
-            if use_last_config == "Y":
-                config = configparser.ConfigParser()
-                config.read("config.ini")
-                cluster_name = config["DEFAULT"]["cluster_name"]
-                task_name = config["DEFAULT"]["task_name"]
-                container_name = config["DEFAULT"]["container_name"]
-                profile_name = config["DEFAULT"]["profile_name"]
-                command = f'aws ecs execute-command --cluster {cluster_name} --task {task_name} --container {container_name} --command "/bin/bash" --interactive --profile {profile_name}'
-                subprocess.run(command, shell=True)
+        credentials_type = make_choice(choice="credentials_type")
+        if credentials_type == "AWS_CREDENTIALS_FILE":
+            profile_name = make_choice(choice="profile_name")
+        else:
+            profile_name = credentials_type
+            os.environ["AWS_DEFAULT_REGION"] = make_choice(choice="region_name")
 
-        if (os.path.isfile("config.ini") == True and use_last_config == "N") or (
-            os.path.isfile("config.ini") is False
-        ):
-            credentials_type = make_choice(choice="credentials_type")
-            if credentials_type == "AWS_CREDENTIALS_FILE":
-                profile_name = make_choice(choice="profile_name")
-            else:
-                profile_name = credentials_type
-                os.environ["AWS_DEFAULT_REGION"] = make_choice(choice="region_name")
+        cluster_name = make_choice(choice="cluster_name", profile=profile_name)
 
-            cluster_name = make_choice(choice="cluster_name", profile=profile_name)
+        service_name = make_choice(
+            choice="service_name", profile=profile_name, cluster_name=cluster_name
+        )
 
-            service_name = make_choice(
-                choice="service_name", profile=profile_name, cluster_name=cluster_name
-            )
+        task_name = make_choice(
+            choice="task_arn",
+            profile=profile_name,
+            cluster_name=cluster_name,
+            service_name=service_name,
+        )
 
-            task_name = make_choice(
-                choice="task_arn",
-                profile=profile_name,
-                cluster_name=cluster_name,
-                service_name=service_name,
-            )
+        container_name = make_choice(
+            choice="container_name",
+            profile=profile_name,
+            cluster_name=cluster_name,
+            task_name=task_name,
+        )
 
-            container_name = make_choice(
-                choice="container_name",
-                profile=profile_name,
-                cluster_name=cluster_name,
-                task_name=task_name,
-            )
+        print(f"Connection to {container_name} ...")
+        command = f'aws ecs execute-command --cluster {cluster_name} --task {task_name} --container {container_name} --command "/bin/bash" --interactive'
 
-            print(f"Connection to {container_name} ...")
-            command = f'aws ecs execute-command --cluster {cluster_name} --task {task_name} --container {container_name} --command "/bin/bash" --interactive'
+        if profile_name != "EC2_INSTANCE_METADATA":
+            command = f"{command} --profile {profile_name}"
 
-            config = configparser.ConfigParser()
-            config["DEFAULT"] = {
-                "cluster_name": f"{cluster_name}",
-                "task_name": f"{task_name}",
-                "container_name": f"{container_name}",
-            }
-            with open("config.ini", "w") as configfile:
-                config.write(configfile)
-
-            if profile_name != "EC2_INSTANCE_METADATA":
-                command = f"{command} --profile {profile_name}"
-                config["DEFAULT"]["profile_name"] = f"{profile_name}"
-                with open("config.ini", "w") as configfile:
-                    config.write(configfile)
-
-            subprocess.run(command, shell=True)
+        subprocess.run(command, shell=True)
 
     except (KeyboardInterrupt, TypeError) as e:
         print("Bye bye !")
@@ -259,8 +232,13 @@ def exec_command(
         subprocess.run(command, shell=True)
 
     except (KeyboardInterrupt, TypeError) as e:
-        print("Bye bye !")
-        sys.exit()
+        try:
+            time.sleep(2)
+        except (KeyboardInterrupt, TypeError) as e:
+            print("Bye bye !")
+            sys.exit()
+        else:
+            pass
 
 
 def _version_callback(value: bool) -> None:
